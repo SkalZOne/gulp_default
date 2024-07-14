bcab4051a43e6f0b7e35dd77f9b4d516598ad788
SkalZ <dima.tysyahnyy@mail.ru>
2024-07-14 18:29:25 +0500

    imgmin rework

--- gulp/tasks/imgmin.js
+++ gulp/tasks/imgmin.js
@@ -1,12 +1,10 @@
 import gulp from 'gulp';
-import imagemin from 'gulp-imagemin';
 import browserSync from 'browser-sync';
 import webp from 'gulp-webp'
 import { deleteAsync } from 'del'
 
 export const imageMin = () => {
     return gulp.src('site/src/img/*', {encoding: false})
-        .pipe(imagemin())
         .pipe(webp())
         .pipe(gulp.dest('site/dest/img'))
         .pipe(browserSync.stream())
